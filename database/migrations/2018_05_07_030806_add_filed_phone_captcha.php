b9fa0dc64fcd2899f95034c16c572841db83d44e
huyoujin <Hu1329489264@163.com>
2018-05-07 11:13:30 +0800

    fix

--- database/migrations/2018_05_07_030806_add_filed_phone_captcha.php
+++ database/migrations/2018_05_07_030806_add_filed_phone_captcha.php
@@ -13,7 +13,7 @@ class AddFiledPhoneCaptcha extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('phone_captcha');
         });
     }
 
@@ -25,7 +25,7 @@ class AddFiledPhoneCaptcha extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('phone_captcha');
         });
     }
 }
